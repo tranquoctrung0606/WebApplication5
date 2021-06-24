fb09a3991fb6f4e6bfeb560d9fe4b3d8d3118673
tqtrung2 <tqtrung2@TQTRUNG2>
2021-06-24 11:35:37 +0700

    Add project files.

---
+++ WebApplication5/Data/WebApplication5Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace WebApplication5.Data
+{
+    public class WebApplication5Context : DbContext
+    {
+        public WebApplication5Context (DbContextOptions<WebApplication5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+        public DbSet<MvcMovie.Models.User> User { get; set; }
+    }
+}
---
+++ WebApplication5/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+    }
+}
